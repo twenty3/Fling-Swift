617b057c35567b1a612d1daea65466ca7438d8e7
Chris Parrish <twenty3@ShinySpace.local>
2015-05-02 10:16:37 -0700

    Initial commit - default project

---
+++ Fling Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fling Swift
+//
+//  Created by Chris Parrish on 5/2/15.
+//  Copyright (c) 2015 Chris Parrish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
