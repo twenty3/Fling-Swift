73cb11f273b557f6ff50fd181ba17acd12a5ef41
Chris Parrish <twenty3@ShinySpace.local>
2015-05-02 19:48:25 -0700

    Add the ability to drag tokens around with an attachment behavior.

--- Fling Swift/PItchViewController.swift
+++ Fling Swift/PItchViewController.swift
@@ -15,12 +15,19 @@ class PItchViewController: UIViewController {
     private lazy var fieldCollisionBehavior: UICollisionBehavior = self.initFieldCollisionBehavior()
     private lazy var tokenCollisionBehavior: UICollisionBehavior = self.initTokenCollisionBehavior()
     
+    private var dragAttachmentBehavior: UIAttachmentBehavior? = nil
+    private var dragStartingPoint: CGPoint = CGPointZero
+    
     private lazy var tokenViews: [TokenView] = {
             let tokenLabels = ["1", "2", "3", "4", "5", "6̲", "7", "8", "9̲", "10", "11"]
             
             var tokens = [TokenView]()
             for labelString in tokenLabels {
                 let tokenView = TokenView(label: labelString)
+                
+                let dragRecognizer = UIPanGestureRecognizer(target: self, action: "tokenDidPan:")
+                tokenView.addGestureRecognizer(dragRecognizer)
+                
                 tokens.append(tokenView)
             }
             
@@ -92,18 +99,53 @@ class PItchViewController: UIViewController {
     
     //MARK: - Behaviors
     
-    func addInitialBehaviors() {
+    private func addInitialBehaviors() {
         for tokenView in self.tokenViews {
             self.animator.addBehavior(tokenView.dynamicBehavior)
         }
         self.animator.addBehavior(self.tokenCollisionBehavior)
     }
     
-    func addCollectAtBottomBehaviors() {
+    private func addCollectAtBottomBehaviors() {
         self.animator.addBehavior(self.gravityBehavior)
         println("+ Added Gravity Behavior")
         self.animator.addBehavior(self.fieldCollisionBehavior)
         println("+ Added Collision Behavior")
+    }
+    
+    func tokenDidPan(panRecognizer: UIPanGestureRecognizer)
+    {
+        // We'll only add the attachment for dragging, when a drag starts
+        // otherwise the attachment will hold the item in place even
+        // when we are not moving it.
+        if panRecognizer.state == UIGestureRecognizerState.Began {
+            
+            let viewToDrag = panRecognizer.view as! TokenView
+
+            // We'll add the attachment at the center of the view
+            // and use the delta during each pan gesture update to adjust the attachment location
+            // Using the center will keep the view from rotating around the touched point
+            self.dragStartingPoint = viewToDrag.center
+            self.dragAttachmentBehavior = UIAttachmentBehavior(item: viewToDrag, attachedToAnchor: viewToDrag.center)
+            self.animator.addBehavior(self.dragAttachmentBehavior)
+        }
+        else if panRecognizer.state == UIGestureRecognizerState.Ended {
+            self.dragStartingPoint = CGPointZero;
+            self.animator.removeBehavior(self.dragAttachmentBehavior)
+            self.dragAttachmentBehavior = nil
+        }
+        else if panRecognizer.state == UIGestureRecognizerState.Changed {
+            // apply the change in the pan recognizer position (cumulative) to the
+            // the starring part to move our token
+            
+            // NOTE: if we do the clever trick of reseting the pan recognizer's
+            // translation to zero here, it will reset the velocity!
+            let delta = panRecognizer.translationInView(self.view)
+            let newPoint = CGPoint(x: self.dragStartingPoint.x + delta.x, y: self.dragStartingPoint.y + delta.y)
+            if let dragAttachmentBehavior = self.dragAttachmentBehavior {
+                dragAttachmentBehavior.anchorPoint = newPoint
+            }
+        }
     }
     
     //MARK: - Debug
